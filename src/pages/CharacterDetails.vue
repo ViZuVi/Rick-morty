83dce95fbccebd177d1be28d242e89b48fdaa5f5
Zu <zb@dot-dot.ru>
2022-03-28 16:07:53 +0300

    apollo added; character details axios request replaced by graphql query

--- src/pages/CharacterDetails.vue
+++ src/pages/CharacterDetails.vue
@@ -1,29 +1,63 @@
 <template>
   <div class="character">
-    <img :src="characterInfo.image" alt="" />
-    <div class="character__wrapper">
-      <h3>{{ characterInfo.name }}</h3>
+    <div v-if="$apollo.loading">Loading...</div>
+    <div v-else-if="$apollo.error">An error occured</div>
+    <div v-else>
+      <img class="character__img" :src="characterInfo.image" :alt="characterInfo.name" />
+      <div class="character__wrapper">
+        <h3>{{ characterInfo.name }}</h3>
+        <p>{{ characterInfo.species }}</p>
+        <p>{{ characterInfo.status }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import gql from "graphql-tag";
 
 export default {
   name: "CharacterDetails",
+  apollo: {
+    // #1
+
+    // characterInfo: gql`
+    //   query {
+    //     characterInfo: character(id: 1) {
+    //       id
+    //       name
+    //       image
+    //       status
+    //       species
+    //     }
+    //   }
+    // `,
+
+    // #2
+    characterInfo() {
+      return {
+        query: gql`
+          query getCharacter($id: ID!) {
+            characterInfo: character(id: $id) {
+              id
+              name
+              image
+              status
+              species
+            }
+          }
+        `,
+        variables: {
+          id: this.id,
+        },
+      };
+    },
+  },
   data() {
     return {
+      id: this.$route.params.id,
       characterInfo: {},
     };
-  },
-  async created() {
-    const id = this.$route.params.id;
-    await axios
-      .get(`https://rickandmortyapi.com/api/character/${id}`)
-      .then(({data}) => {
-        this.characterInfo = data;
-      });
   },
 };
 </script>
@@ -31,5 +65,11 @@ export default {
 <style>
 .character {
   padding: 20px;
+}
+.character__img {
+  display: inline-block;
+  min-width: 300px;
+  min-height: 300px;
+  background-color: rgb(94, 105, 64);
 }
 </style>
